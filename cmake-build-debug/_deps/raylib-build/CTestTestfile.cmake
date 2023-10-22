c527028a08517d33dbfa7320fad12b654a81da4a
Antonius Joshua <antoniusjoshua47@gmail.com>
2023-10-22 19:40:56 +0700

    feat: update ignore

--- cmake-build-debug/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/joshua/Learning/c_plus/learning_game_dev/cmake-build-debug/_deps/raylib-src
-# Build directory: /Users/joshua/Learning/c_plus/learning_game_dev/cmake-build-debug/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
